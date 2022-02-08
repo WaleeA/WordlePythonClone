2de31fd512579ccfdf5269598ca3f401431703e3
Walee <58791789+WaleeA@users.noreply.github.com>
2022-02-08 14:28:12 +0000

    first commit

---
+++ wordle.py
@@ -0,0 +1,44 @@
+import random
+dictout = open("dict.txt", "r")
+list_dict = [(line.strip()).split() for line in dictout]
+dictout.close()
+def check(attempt_break, y): 
+    # returns a list of booleans
+    result = [characters in y for characters in attempt_break]
+    return result
+def menu():
+    print("Welcome to wordle")
+    menuinput = int(input("Enter 1 to start or 2 to Exit")) #Menu point
+    while menuinput != 2:
+        if menuinput == 1:
+            maindef() #start main game after pressing 1
+        elif menuinput != 1 or 3:
+            print("wrong input")
+            menuinput = int(input("Enter 1 to start or 2 to Exit"))
+    if menuinput == 2:
+        quit()
+def maindef():
+    print("Begin game")
+    rand_dict = random.choice(list_dict)
+    #print(rand_dict)
+    y = list(rand_dict)
+    empty_array = []
+    #print (y)
+    counter = 1
+    while counter != 6:
+        attempt = str(input("Enter your guess")).upper()
+        attempt_break = list(attempt)
+        if attempt_break == y:
+            print(check(attempt_break,y))
+            print("Correct! You've got in", counter, "Attempts")
+        else:
+            print("wrong")
+            print(check(attempt_break,y))
+        counter += 1
+    menu()
+###Main running code###
+menu()
+
+
+
+
